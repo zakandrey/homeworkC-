2f75af21358e82b6a3b091631429995482dc7daa
ZakirovAndrey <zak.andrey94@gmail.com>
2022-12-18 16:58:09 +0300

    Третья задача

---
+++ Task003/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+    if (number % 2 == 0)
+    {
+        System.Console.WriteLine("да");
+    }
+    else
+    {   
+        System.Console.WriteLine("нет");
+    }
+
