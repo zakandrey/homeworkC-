1ff4580db4d9fac63f51cf0d7d078ea4256cbf97
ZakirovAndrey <zak.andrey94@gmail.com>
2022-12-18 16:52:28 +0300

    Четвертая задача

---
+++ Task004/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int evenNum = 2;
+    if (number > 1)
+    {
+    while (evenNum<=number)
+    {
+        System.Console.WriteLine(evenNum + " ");
+        evenNum = evenNum + 2;
+    }
+    }
