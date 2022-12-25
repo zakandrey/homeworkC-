41d389d4b4710990881a1374103041a384101cbb
ZakirovAndrey <zak.andrey94@gmail.com>
2022-12-26 01:13:31 +0300

    Седьмая задача

---
+++ Task007/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введи число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 1 || number > 7)
+{
+System.Console.WriteLine("Такого дня недели нет");
+return;
+}
+if (number >= 1 && number <= 5)
+{
+    System.Console.WriteLine("Будний день");
+
+}
+else
+{
+    System.Console.WriteLine("Выходной день");
+}
