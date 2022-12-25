5f79d551f92321c8c1ddd6c055b49e3e2d3a929b
ZakirovAndrey <zak.andrey94@gmail.com>
2022-12-26 00:37:37 +0300

    Шестое задание

---
+++ Task006/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введи число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+    if (number / 100 != 0)
+{
+    while (number > 999)
+    {
+        number /= 10;
+    }
+    int answer = number % 10;
+    System.Console.WriteLine(answer);
+}
+else 
+    System.Console.WriteLine("Третьей цифры нет");
