42de9d6672da6d93faaf18926c0b437ec82dface
ZakirovAndrey <zak.andrey94@gmail.com>
2022-12-28 22:01:09 +0300

    Восьмая задача

---
+++ Task008/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: ");
+string number = Console.ReadLine();
+if (number.Length !=5)
+{
+    System.Console.WriteLine("Введите пятизначное число");
+    return;
+}
+if (number[0]==number[4] && number[1]==number[3])
+{
+    System.Console.WriteLine("Палиндром");
+}
+else {
+    System.Console.WriteLine("Не палиндром");
+}
