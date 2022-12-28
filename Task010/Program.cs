585a27007db3073c4385630f75885f7a8e237228
ZakirovAndrey <zak.andrey94@gmail.com>
2022-12-29 00:02:17 +0300

    Десятая задача

---
+++ Task010/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= number; i++)
+{
+    System.Console.WriteLine(Math.Pow(i,3));
+}
