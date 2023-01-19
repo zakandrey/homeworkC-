3485ffd7d594c129ffcccbfa8f6024988340f1f0
ZakirovAndrey <zak.andrey94@gmail.com>
2023-01-19 12:45:24 +0300

    Одиннадцатая задача

---
+++ Task011/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+Console.Write("Введите певрое число: ");
+double NumberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+double NumberB = Convert.ToInt32(Console.ReadLine());
+double result = 0;
+for (int i = 1; i <= NumberB; i++)
+
+result = Math.Pow(NumberA, NumberB);
+
+{
+    System.Console.WriteLine(result);
+}
