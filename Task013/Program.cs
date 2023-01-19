f6beb9d002ed5211e65b2d77315b8c6a99f31cd0
ZakirovAndrey <zak.andrey94@gmail.com>
2023-01-19 19:33:32 +0300

    Тринадцатая задача

---
+++ Task013/Program.cs
@@ -0,0 +1,15 @@
+﻿int [] array = new int[8];
+Console.Write("[");
+
+for (int i = 0; i < array.Length; i++)
+ {
+    array [i] = new Random().Next(0, 100);
+ }
+
+    Console.Write(GetArrayInString(array));
+    Console.Write("]");
+
+string GetArrayInString (int [] array)
+{
+    return String.Join(",", array);
+}
