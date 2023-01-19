a7763831e21422d3c4944b4f2119ef9d777fc4ba
ZakirovAndrey <zak.andrey94@gmail.com>
2023-01-19 18:28:24 +0300

    Двенадцатая задача

---
+++ Task012/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+Console.Write("Введи число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+int Summ = 0;
+int result = 0;
+for (int i = 0; Number > 0;i++)
+{
+Summ = Number % 10;
+result += Summ;
+Number = Number / 10;
+}
+{
+    System.Console.WriteLine(result);
+}
