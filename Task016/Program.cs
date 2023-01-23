4d4609ee133ad83090041b3632acd3a867b21b4a
ZakirovAndrey <zak.andrey94@gmail.com>
2023-01-23 19:20:22 +0300

    Шестнадцатая задача

---
+++ Task016/Program.cs
@@ -0,0 +1,38 @@
+﻿//  Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+double [] numbers = new double[size];
+GetArrayInString(numbers);
+FillArrayRandomNumbers(numbers);
+double max = 0;
+double min = 100;
+
+string GetArrayInString (double [] numbers)
+{
+    return String.Join(",", numbers);
+}
+Console.WriteLine(GetArrayInString(numbers));
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > max )
+    {
+        max = numbers[i];
+    }
+    if (numbers[i] < min)
+    {
+        min = numbers[i];
+    }
+}
+Console.WriteLine($"всего {numbers.Length} чисел. Максимальное число = {max}, минимальное число = {min}");
+Console.WriteLine($"Разница между максимальным и минимальным числом = {max - min}");
+
+void FillArrayRandomNumbers(double[] numbers)
+{
+for (int i = 0; i < numbers.Length; i++)
+ {
+    numbers [i] = new Random().Next(1, 100);
+ }
+}
