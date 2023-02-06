35aac50671c8d91149d6524f2e854fc91f3bbd1c
ZakirovAndrey <zak.andrey94@gmail.com>
2023-02-06 03:11:39 +0300

    Двадцать шестая задача

---
+++ Task026/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int Naturalnumber (int number)
+{
+    if (number == 1) 
+    {
+        return 1;
+    }
+    else 
+    {
+        Console.Write ($"{number}, ");
+        number = Naturalnumber (number - 1);
+    }
+        return number;
+}
+
+int number = InputNumber ("Введите число N: ");
+Console.WriteLine(Naturalnumber (number));
+
+int InputNumber (string message)
+{
+    Console.Write (message);
+    return Convert.ToInt32 (Console.ReadLine ());
+}
